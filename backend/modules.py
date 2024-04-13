233b936e2ea87415d0fb0182f10e6e53b09c26d9
MetroCat69 <aaaaa>
2024-04-13 19:14:54 +0300

    updated backend project structure

--- backend/modules.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel
-
-from mongoengine import Document, StringField, IntField
-
-class User(Document):
-    name = StringField(required=True)
-    age = IntField(required=True)
-    email = StringField()
